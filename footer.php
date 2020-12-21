aa5bb4f58067cc9fe302e3d90dcc3b8a2c325e84
doublezed2 <doublezed2@gmail.com>
2020-12-21 15:23:25 +0500

    removed all old files

--- footer.php
+++
@@ -1,8 +0,0 @@
-    <script src="assets/jquery-3.5.1.min.js"></script>
-    <script src="assets/jquery-ui.min.js"></script>
-    <script src="assets/bs/js/bootstrap.min.js"></script>
-    <script src="js/jquery.mask.js"></script>
-    <script src="js/main.js?v=1.8"></script>
-
-  </body>
-</html>
--- top-nav.php
+++
@@ -1,8 +0,0 @@
-<nav class="navbar navbar-dark sticky-top bg-primary flex-md-nowrap p-0">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#"><img width="55px" src="img/logo.jpg" alt="logo"></a>
-    <ul class="navbar-nav px-3">
-    <li class="nav-item text-nowrap">
-        <a class="nav-link" href="logout.php"><?php echo $_SESSION["user_name"];?> - Sign out</a>
-    </li>
-    </ul>
-</nav>
